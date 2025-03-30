073909438951327577f506567690c23b3158cc58
Aryan-Khanijo <akhanijo12345@gmail.com>
2025-03-30 21:07:37 +0530

    FIX: local dep changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.REACT_APP_URL}/api`,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `/api`,
   withCredentials: true,
 });
